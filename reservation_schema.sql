8def7bb5e4532ab7034f0dc3c361df6c0644fbb5
Viteszeg <rofi_santos@dlsu.edu.ph>
2016-10-10 00:43:39 +0800

    Initial Database : Added More Crucial Info

--- reservation_schema.sql
+++ reservation_schema.sql
@@ -19,21 +19,46 @@ CREATE TABLE `reservation_system`.`computers` (
     ON DELETE NO ACTION
     ON UPDATE NO ACTION);
 
+CREATE TABLE `reservation_system`.`colleges` (
+  `collegeid` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`collegeid`));
+
+CREATE TABLE `reservation_system`.`types` (
+  `typeid` INT NOT NULL,
+  `type` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`typeid`));
+
+
 CREATE TABLE `reservation_system`.`reservations` (
   `reservationid` INT NOT NULL AUTO_INCREMENT,
   `computerid` INT NOT NULL,
   `useridno` INT NOT NULL,
   `reservedatetime` DATETIME NOT NULL,
-  `college` VARCHAR(45) NOT NULL,
+  `collegeid` INT NOT NULL,
+  `typeid` INT NOT NULL,
   `verified` BIT NOT NULL,
   `verificationcode` VARCHAR(32) NOT NULL,
   PRIMARY KEY (`reservationid`),
+  INDEX `typeid_idx` (`typeid` ASC),
+  INDEX `collegeid_idx` (`collegeid` ASC),
   INDEX `computerid_idx` (`computerid` ASC),
   CONSTRAINT `computerid`
     FOREIGN KEY (`computerid`)
     REFERENCES `reservation_system`.`computers` (`computerid`)
     ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `typeid`
+    FOREIGN KEY (`typeid`)
+    REFERENCES `reservation_system`.`types` (`typeid`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `collegeid`
+    FOREIGN KEY (`collegeid`)
+    REFERENCES `reservation_system`.`colleges` (`collegeid`)
+    ON DELETE NO ACTION
     ON UPDATE NO ACTION);
+
 
 
 /*dummy data*/
@@ -63,3 +88,19 @@ VALUES (1, 1),
 	   (8, 2),
 	   (9, 2),
 	   (10, 2);
+
+INSERT INTO `reservation_system`.`colleges` (`name`)
+VALUES ("College of Business"),
+	   ("College of Computer Science"),
+	   ("College of Engineering"),
+	   ("School of Economics"),
+	   ("College of Law"),
+	   ("College of Liberal Arts"),
+	   ("College of Science");
+
+INSERT INTO `reservation_system`.`types` (`name`)
+VALUES ("Senior High School"),
+	   ("Undergraduate"),
+	   ("Graduate"),
+	   ("Faculty"),
+	   ("Staff");
