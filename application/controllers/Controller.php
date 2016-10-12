77aa9842cd66a1413fde67f5a3db801aab7813f8
Patrick Tobias <patrick_tobias@outlook.com>
2016-10-12 11:16:00 +0800

    Merge steps into home.php

--- application/controllers/Controller.php
+++ application/controllers/Controller.php
@@ -1,6 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-include_once ('/../views/InnerViews/InnerViewList.php');
 
 class Controller extends CI_Controller {
 
--- application/views/home.php
+++ application/views/home.php
@@ -53,11 +53,250 @@ $defaultTab = 1;
 
             $tab = (isset($tab)) ? $tab : 'tab' . $defaultTab;
 
-            $this->load->view('InnerViews/step1.php', $step1);
-            $this->load->view('InnerViews/step2.php', $step1);
-            $this->load->view('InnerViews/step3.php', $step1);
+            //$this->load->view('InnerViews/step1.php', $step1);
+            //$this->load->view('InnerViews/step2.php', $step1);
+            //$this->load->view('InnerViews/step3.php', $step1);
             ?>
-        </div>
+
+            <?php
+            /**
+             * Created by PhpStorm.
+             * User: Patrick
+             * Date: 10/8/2016
+             * Time: 3:41 PM
+             */
+
+            $stepNo = 1;
+
+            ?>
+
+            <script type = "text/javascript">
+
+                $(function () { // put functions in respective buttons
+
+                    $('.pager li.nextStep_<?php echo $stepNo ?>').on('click', function () { // for next step
+                        if ($(this).hasClass('active'))
+                            $(this).removeClass('active');
+
+                        $("#tabs li.tab_<?php echo $stepNo ?>").removeClass('active');
+                        $("#tabs li.tab_<?php echo $stepNo ?>").addClass('disabled');
+
+                        $("#tabs li.tab_<?php echo $stepNo+1 ?>").addClass('active');
+                    });
+
+                });
+
+            </script>
+
+            <div id = "tab_1_<?php echo $stepNo ?>" class="tab-pane fade in <?php echo ($tab == $stepNo) ? 'active' : ''; ?>">
+
+                <div class = "row">
+                    <div class = "col-md-3 col-md-offset-1">
+                        <div class = "panel panel-default">
+                            <div class = "panel-body">
+                                <div class="radio">
+                                    <div class="radio">
+                                        <label><input type="radio" name="optradio">Today (<?=date("m-d-Y")?>)</label>
+                                    </div>
+                                    <div class="radio">
+                                        <label><input type="radio" name="optradio">Tomorrow (<?=date("m-d-Y", strtotime("tomorrow"))?>)</label>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class = "panel panel-default">
+                            <div class = "panel-body">
+                                Building:
+                                <select name="form-building">
+                                    <option value="" disabled selected>Select...</option>
+                                    <?php foreach($buildings as $row):?>
+                                        <option value="<?=$row->buildingid?>"><?=$row->name?></option>
+                                    <?php endforeach;?>
+                                </select>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class = "col-md-7">
+                        <div class = "panel panel-default">
+                            <div class = "panel-body">
+                                SLOTS
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class ="row">
+
+                    <div class = "col-md-3 col-md-offset-8">
+                        <ul class="pager">
+                            <!--<li class="previous prevStep_</?php echo $stepNo ?>">
+                                <a href="#tab_1_</?php echo $stepNo-1 ?>" data-toggle="tab"><span aria-hidden="true">&larr;</span> Go back to previous step</a>
+                            </li>-->
+                            <li class="nextStep_<?php echo $stepNo ?>">
+                                <a href="#tab_1_<?php echo $stepNo+1 ?>" data-toggle="tab">Proceed to next step <span aria-hidden="true">&rarr;</span></a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <?php
+            /**
+             * Created by PhpStorm.
+             * User: Patrick
+             * Date: 10/8/2016
+             * Time: 3:41 PM
+             */
+
+            $stepNo++;
+
+            ?>
+
+            <script type = "text/javascript">
+
+                $(function () { // put functions in respective buttons
+
+                    $('.pager li.nextStep_<?php echo $stepNo ?>').on('click', function () { // for next step
+                        if ($(this).hasClass('active'))
+                            $(this).toggleClass('active');
+
+                        $("#tabs li.tab_<?php echo $stepNo ?>").removeClass('active');
+                        $("#tabs li.tab_<?php echo $stepNo ?>").addClass('disabled');
+
+                        $("#tabs li.tab_<?php echo $stepNo+1 ?>").addClass('active');
+                    });
+
+                    $('.pager li.prevStep_<?php echo $stepNo ?>').on('click', function () { // for next step
+                        if ($(this).hasClass('active'))
+                            $(this).toggleClass('active');
+
+                        $("#tabs li.tab_<?php echo $stepNo ?>").removeClass('active');
+                        $("#tabs li.tab_<?php echo $stepNo ?>").addClass('disabled');
+
+                        $("#tabs li.tab_<?php echo $stepNo-1 ?>").addClass('active');
+                    });
+
+                });
+
+            </script>
+
+            <div id = "tab_1_<?php echo $stepNo ?>" class="tab-pane fade in <?php echo ($tab == $stepNo) ? 'active' : ''; ?>">
+
+                <div class = "row">
+                    <div class = "panel-body">
+                        <div class = "col-md-3 col-md-offset-2">
+                            <form>
+                                <div class="form-group">
+                                    <label for="idno">ID Number:</label>
+                                    <input type="number" class="form-control" id="idno">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="college">College:</label> <!-- THIS SHOULDN'T BE HARDCODED -->
+                                    <select class="form-control" id="college">
+                                        <option selected disabled>Choose your college...</option>
+                                        <option>CCS</option>
+                                        <option>COB</option>
+                                        <option>COS</option>
+                                        <option>CLA</option>
+                                    </select>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="type">Type:</label>
+                                    <select class="form-control" id="type"> <!-- THIS SHOULDN'T BE HARDCODED -->
+                                        <option selected disabled>Choose your type...</option>
+                                        <option>Student</option>
+                                        <option>Faculty</option>
+                                    </select>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="email">Email:</label>
+                                    <input type="email" class="form-control" id="email">
+                                </div>
+
+                                <b>Date:</b> mm/dd/yyyy
+                                <br /><br />
+                                <b>Time Slots:</b>
+                                <div class = "row">
+                                    <div class = "col-md-6">
+                                        <div class="form-group">
+                                            <label for="starttime">Start:</label>
+                                            <input type="starttime" class="form-control" id="starttime">
+                                        </div>
+                                    </div>
+                                    <div class = "col-md-6">
+                                        <div class="form-group">
+                                            <label for="endtime">End:</label>
+                                            <input type="endtime" class="form-control" id="endtime">
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+
+                <div class ="row">
+
+                    <div class = "col-md-10 col-md-offset-1">
+                        <ul class="pager">
+                            <li class="previous prevStep_<?php echo $stepNo ?>">
+                                <a href="#tab_1_<?php echo $stepNo-1 ?>" data-toggle="tab"><span aria-hidden="true">&larr;</span> Go back to previous step</a>
+                            </li>
+                            <li class="next nextStep_<?php echo $stepNo ?>">
+                                <a href="#tab_1_<?php echo $stepNo+1 ?>" data-toggle="tab">Proceed to next step <span aria-hidden="true">&rarr;</span></a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <?php
+            /**
+             * Created by PhpStorm.
+             * User: Patrick
+             * Date: 10/8/2016
+             * Time: 3:41 PM
+             */
+
+            $stepNo++;
+
+            ?>
+
+            <div id = "tab_1_<?php echo $stepNo ?>" class="tab-pane fade in <?php echo ($tab == $stepNo) ? 'active' : ''; ?>">
+
+                <div class = "row">
+                    <div class = "col-md-10 col-md-offset-1">
+                        <div class="panel-body">
+                            STEP3
+                        </div>
+                    </div>
+                </div>
+
+                <!--<div class ="row">
+
+                    <div class = "col-md-3 col-md-offset-8">
+                        <ul class="pager">
+                            <li class="previous"><a href="#"><span aria-hidden="true">&larr;</span> Older</a></li>
+                            <li class="next"><a href="#">Proceed to next step <span aria-hidden="true">&rarr;</span></a></li>
+                        </ul>
+                    </div>
+
+                </div>-->
+
+            </div>
+
+
+        </div> <!-- EOF -->
 
     </div>
 
