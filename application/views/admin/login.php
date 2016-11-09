d2c54478bf2ccf2becc0826db1acab944a813eef
Vincent Ortega <Vincent Ortega>
2016-11-10 00:23:03 +0800

    FIXED Weird Login Bug

--- application/views/admin/login.php
+++ application/views/admin/login.php
@@ -5,14 +5,14 @@
         <img src="http://tinypic.com/evb0ut.jpg">
         <div class="form-group">
             <label for="adminEmail">Email:</label>
-            <input type="email" class="form-control" id="adminEmail" placeholder="Ex. 123@dlsu.edu.ph" required>
+            <input type="email" class="form-control" name="adminEmail" id="adminEmail" placeholder="Ex. 123@dlsu.edu.ph" required>
         </div>
 
         <BR><BR>
 
         <div class="form-group">
             <label for="adminPassword">Password:</label>
-            <input type="password" class="form-control" id="adminPassword" placeholder="Enter Your Password Here" required>
+            <input type="password" class="form-control" name="adminPassword" id="adminPassword" placeholder="Enter Your Password Here" required>
         </div>
 
         <BR><BR>
