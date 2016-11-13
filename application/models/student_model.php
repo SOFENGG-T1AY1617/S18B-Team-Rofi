49d57e18c70b1f790cf961b819c9f37b281b6247
Patrick Tobias <patrick_tobias@outlook.com>
2016-11-13 17:02:36 +0800

    Add a function that queries the slot limit of a student based on student ID

--- application/models/student_model.php
+++ application/models/student_model.php
@@ -263,4 +263,15 @@ class Student_Model extends CI_Model
 
         return $this->db->query($sql, array($id))->result();
     }
+
+    function getSlotLimitOfCurrentStudentID($id) {
+        $sql = "SELECT MIN(b.limit) as 'minLimit'
+                FROM (SELECT br.limit
+                      FROM business_rules br NATURAL JOIN (SELECT DISTINCT departmentid
+                                                        FROM departments NATURAL JOIN (SELECT DISTINCT departmentid
+                                                                                 FROM rooms NATURAL JOIN (SELECT DISTINCT roomid 
+                                                                                             FROM computers NATURAL JOIN (SELECT DISTINCT computerid
+                                                                                                                          FROM reservations
+                                                                                                                          WHERE useridno = ?) res ) c ) ro ) d) b";
+    }
 }
