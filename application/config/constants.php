9588e63b79a33496b5fcf2c4bc307c2953e5f22c
Viteszeg <rofi_santos@dlsu.edu.ph>
2016-11-30 18:20:52 +0800

    DATABASE UPDATE : ADDED TAG SYSTEM ROOM AND MOD TABLE

--- application/config/constants.php
+++ application/config/constants.php
@@ -154,6 +154,10 @@ define("COLUMN_CONFIRMATION_EXPIRY", "confirmation_expiry");
 define("COLUMN_START_TIME", "start_time");
 define("COLUMN_END_TIME", "end_time");
 
+define("TABLE_TAG_MOD_ROOMS", "tag_mod_rooms");
+define("COLUMN_TAG_MOD_ROOMSID", "tag_mod_roomsid");
+define("COLUMN_MODERATORID", "moderatorid");
+define("COLUMN_ROOMID", "roomid");
 
 /* ADMIN PAGES */
 define("ADMIN_SCHEDULING", "scheduling");
