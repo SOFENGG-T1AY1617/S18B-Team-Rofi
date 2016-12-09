e7c2cd9cc3a68fe5fd790916c48c7987658aed85
Viteszeg <rofi_santos@dlsu.edu.ph>
2016-12-09 23:52:43 +0800

    DATABASE UPDATE : add closed pcs table, merge SPs with schema to import once only

--- reservation_schema.sql
+++ reservation_schema.sql
@@ -215,8 +215,8 @@ CREATE TABLE `reservation_system`.`archive_reservations` (
 
 CREATE TABLE `reservation_system`.`tag_mod_rooms` (
   `tag_mod_roomsid` INT NOT NULL AUTO_INCREMENT,
-  `moderatorid` INT NOT NULL,
-  `roomid` INT NOT NULL,
+  `moderatorid` INT NOT NULL UNIQUE,
+  `roomid` INT NOT NULL UNIQUE,
   PRIMARY KEY (`tag_mod_roomsid`),
   INDEX `moderatorid_idx` (`moderatorid` ASC),
   INDEX `roomid_idx` (`roomid` ASC),
@@ -230,6 +230,20 @@ CREATE TABLE `reservation_system`.`tag_mod_rooms` (
 	REFERENCES `reservation_system`.`rooms` (`roomid`)
 	ON DELETE NO ACTION
 	ON UPDATE NO ACTION);
+
+CREATE TABLE `reservation_system`.`closed_pcs` (
+  `closed_pcsid` INT NOT NULL AUTO_INCREMENT,
+  `computerid` INT NOT NULL,
+  `start_datetime` DATETIME NOT NULL,
+  `end_datetime` DATETIME NOT NULL,
+  PRIMARY KEY (`closed_pcsid`),
+  INDEX `computerid_idx` (`computerid` ASC),
+  CONSTRAINT `closed_computerid`
+	FOREIGN KEY (`computerid`)
+	REFERENCES `reservation_system`.`computers` (`computerid`)
+	ON DELETE NO ACTION
+	ON UPDATE NO ACTION);
+
 /*dummy data*/
 INSERT INTO `reservation_system`.`email_extension`(`email_extension`)
 VALUES ("dlsu.edu.ph"),
@@ -385,3 +399,105 @@ INSERT INTO `reservation_system`.`tag_mod_rooms` (`moderatorid`,`roomid`)
 VALUES (1,1),
 	   (2,2),
 	   (3,4);
+
+INSERT INTO `reservation_system`.`closed_pcs` (`computerid`, `start_datetime`, `end_datetime`)
+VALUES (1, "2016-12-10 14:00:00", "2016-12-10 14:00:00"),
+	   (2, "2016-12-10 14:00:00", "2016-12-10 14:00:00"),
+	   (3, "2016-12-10 14:00:00", "2016-12-10 14:00:00"),
+	   (4, "2016-12-10 14:00:00", "2016-12-10 14:00:00"),
+	   (5, "2016-12-10 14:00:00", "2016-12-10 14:00:00"),
+	   (11, "2016-12-10 14:00:00", "2016-12-10 14:00:00"),
+	   (12, "2016-12-10 14:00:00", "2016-12-10 14:00:00"),
+	   (13, "2016-12-10 14:00:00", "2016-12-10 14:00:00"),
+	   (14, "2016-12-10 14:00:00", "2016-12-10 14:00:00"),
+	   (15, "2016-12-10 14:00:00", "2016-12-10 14:00:00"),
+	   (21, "2016-12-10 14:00:00", "2016-12-10 14:00:00");
+
+
+/* STORED PROCEDURES */
+USE `reservation_system`;
+DROP procedure IF EXISTS `archive_past_reservations`;
+DROP procedure IF EXISTS `archive_unconfirmed_reservations`;
+DROP procedure IF EXISTS `archive_deleted_reservations_by_room`;
+DROP procedure IF EXISTS `archive_deleted_reservations_by_computer`;
+
+DELIMITER $$
+USE `reservation_system`$$
+-- date format => "YYYY-MM-DD" with quotations
+-- time format => "HH:MM:SS" with quotations
+CREATE PROCEDURE `archive_past_reservations` (IN curdate DATE, IN curtime TIME)
+BEGIN
+	INSERT INTO `reservation_system`.`archive_reservations`
+	(`computerno`, `room_name`, `userid`, `date`, `start_restime`, 
+	`end_restime`, `verificationcode`, `attendance`)
+	SELECT computer.`computerno`, room.`name`,`userid`, `date`, `start_restime`, 
+		   `end_restime`, `verificationcode`, `attendance`
+	FROM `reservation_system`.`reservations` reservation, 
+		 `reservation_system`.`rooms` room, `reservation_system`.`computers` computer
+	WHERE (curdate > `date` OR (curdate = `date` AND curtime >= `end_restime`))
+		  AND reservation.computerid = computer.computerid AND room.roomid = computer.roomid;
+    DELETE FROM `reservation_system`.`reservations`
+    WHERE curdate > `date` OR (curdate = `date` AND curtime >= `end_restime`);
+END$$
+
+
+DELIMITER $$
+USE `reservation_system`$$
+CREATE PROCEDURE `archive_deleted_reservations_by_room` (IN rumid INT)
+BEGIN
+	INSERT INTO `reservation_system`.`archive_reservations`
+	(`computerno`, `room_name`, `userid`, `date`, `start_restime`, 
+	`end_restime`, `verificationcode`, `attendance`)
+	SELECT computer.`computerno`, room.`name`,`userid`, `date`, `start_restime`, 
+		   `end_restime`, `verificationcode`, `attendance`
+	FROM `reservation_system`.`reservations` reservation, 
+		 `reservation_system`.`rooms` room, `reservation_system`.`computers` computer
+	WHERE rumid = room.roomid AND  
+	      reservation.computerid = computer.computerid AND room.roomid = computer.roomid;
+    DELETE FROM `reservation_system`.`reservations`
+    WHERE computerid IN (SELECT c.computerid FROM rooms r,computers c WHERE rumid = r.roomid
+		  AND c.roomid = r.roomid);
+END$$
+
+DELIMITER $$
+USE `reservation_system`$$
+CREATE PROCEDURE `archive_deleted_reservations_by_computer` (IN compid INT)
+BEGIN
+	INSERT INTO `reservation_system`.`archive_reservations`
+	(`computerno`, `room_name`, `userid`, `date`, `start_restime`, 
+	`end_restime`, `verificationcode`, `attendance`)
+	SELECT computer.`computerno`, room.`name`,`userid`, `date`, `start_restime`, 
+		   `end_restime`, `verificationcode`, `attendance`
+	FROM `reservation_system`.`reservations` reservation, 
+		 `reservation_system`.`rooms` room, `reservation_system`.`computers` computer
+	WHERE compid = reservation.computerid AND 
+		  reservation.computerid = computer.computerid AND room.roomid = computer.roomid;
+    DELETE FROM `reservation_system`.`reservations` 
+    WHERE compid = computerid;
+END$$
+
+DELIMITER $$
+USE `reservation_system`$$
+CREATE PROCEDURE `archive_unconfirmed_reservations` (IN confirmation_limit TIME)
+BEGIN
+	INSERT INTO `reservation_system`.`archive_reservations`
+	(`computerno`, `room_name`, `userid`, `date`, `start_restime`, `end_restime`, `verificationcode`, `attendance`)
+	SELECT computer.`computerno`, room.`name`,`userid`, `date`, `start_restime`, 
+		   `end_restime`, `verificationcode`, `attendance`
+	FROM `reservation_system`.`reservations` reservation, 
+		 `reservation_system`.`rooms` room, `reservation_system`.`computers` computer
+	WHERE TIMEDIFF(NOW(), reservation.`time_reserved`) >= confirmation_limit
+		  AND reservation.computerid = computer.computerid AND room.roomid = computer.roomid;
+    DELETE FROM `reservation_system`.`reservations`
+    WHERE TIMEDIFF(NOW(), `time_reserved`) >= confirmation_limit;
+END$$
+
+DELIMITER $$
+USE `reservation_system`$$
+CREATE PROCEDURE `delete_closed_computers` (IN rumid INT)
+BEGIN
+	DELETE FROM `reservation_system`.`closed_pcs`
+	WHERE `computerid` IN (SELECT `computerid` FROM `reservation_system`.`computers` WHERE `roomid` = rumid);
+END$$
+
+DELIMITER ;
