92dd46038dd5b05eb41bf0a590686810e0eb8e0f
Patrick Tobias <patrick_tobias@outlook.com>
2016-12-13 16:22:50 +0800

    Adjust changing of time layout in business rules

--- application/views/admin/a_scheduling.php
+++ application/views/admin/a_scheduling.php
@@ -971,7 +971,7 @@ include 'a_navbar.php';
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
-                <h4 class="modal-title">Disabling Selected Slots</h4>
+                <h4 class="modal-title">Enabling Selected Slots</h4>
             </div>
             <div class="modal-body">
                 Are you sure you want to enable the slots selected?
